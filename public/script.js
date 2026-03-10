755f5ac1c040223871a4f95feba1051a197920a6
v4358137-lang <v4358137@gmail.com>
2026-03-10 16:18:40 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -33,12 +33,27 @@ let username = "";
 const rtcConfig = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
+
     {
       urls: "turn:openrelay.metered.ca:80",
       username: "openrelayproject",
       credential: "openrelayproject"
+    },
+
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    },
+
+    {
+      urls: "turn:openrelay.metered.ca:443?transport=tcp",
+      username: "openrelayproject",
+      credential: "openrelayproject"
     }
-  ]
+  ],
+
+  iceCandidatePoolSize: 10
 };
 
 function addSystemMessage(text) {
